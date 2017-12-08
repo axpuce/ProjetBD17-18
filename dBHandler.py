236822d201f547888b1607eca17090d1f7c80caa
axpuce <suni.1998@hotmail.be>
2017-12-08 15:51:23 +0200

    premiere etape

---
+++ dBHandler.py
@@ -0,0 +1,23 @@
+import sqlite3
+class dBHandler :
+
+	def __init__(self,database):
+		self.conn=sqlite3.connect(database)
+		self.cursor=self.conn.cursor()
+		self.cursor.execute("""CREATE TABLE IF NOT EXISTS funcDep(nameTable TEXT NOT NULL,lhs TEXT NOT NULL,rhs TEXT NOT NULL, PRIMARY KEY(nameTable,lhs,rhs))""")
+		self.conn.commit()
+	
+
+	def insert(self,insert):#insert est un triplet (nameTable,lhs,rhs)
+		self.cursor.execute("""INSERT INTO funcDep(nameTable,lhs,rhs) VALUES(?,?,?)""",insert)
+		self.conn.commit()
+	
+
+	def delete(self,remove):	
+		self.cursor.execute("""DELETE FROM funcDep WHERE nameTable=? AND lhs=? AND rhs=?""",remove)
+		self.conn.commit()
+
+	def select(self,name):
+		self.cursor.execute("SELECT nameTable,lhs,rhs from funcDep WHERE nameTable=?""",name)
+		self.conn.commit()
+	
