8fd7a6e3cf7f7dfe44dd36792363a19ba9f5e93d
Unknown <yacine.sahli@student.umons.ac.be>
2017-12-11 11:25:48 +0100

    cmd implementation

--- dBHandler.py
+++ dBHandler.py
@@ -1,11 +1,14 @@
-import sqlite3
-class dBHandler :
-
-	def __init__(self,database):
+import sqlite3, cmd, sys
+class dBHandler(cmd.Cmd) :
+	intro = 'Welcome to the database handler. type help or ? to list commands/n'
+	prompt = '(dbhandler)'
+	file = None
+	database = ''
+	def connect(self,database):
 		self.conn=sqlite3.connect(database)
 		self.cursor=self.conn.cursor()
 		self.cursor.execute('CREATE TABLE IF NOT EXISTS funcDep(nameTable TEXT NOT NULL,lhs TEXT NOT NULL,rhs TEXT NOT NULL, PRIMARY KEY(nameTable,lhs,rhs))')
-		self.conn.commit()
+		self.conn.commit() 
 	
 
 	def insert(self,insert):#insert est un triplet (nameTable,lhs,rhs)
@@ -19,4 +22,5 @@ class dBHandler :
 	def display(self):
 		for row in cursor.execute("SELECT * from funcDep ORDER BY nameTable"):
 			print(row)
-
+if __name__ == '__main__':
+	dBHandler().cmdloop()
