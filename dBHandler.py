2c3a48532a61b28813e481b69d9a87c5d1fec202
Unknown <yacine.sahli@student.umons.ac.be>
2017-12-11 15:23:34 +0100

    Axelle fait nimp

--- dBHandler.py
+++ dBHandler.py
@@ -8,34 +8,46 @@ class dBHandler(cmd.Cmd) :
 		cmd.Cmd.__init__(self)
 		self.cursor = None
 		self.conn = None
+
 	def do_connect(self,arg):
 		'Attempt to connect to the specified database'
-		self.conn=sqlite3.connect(arg[1])
+		self.conn=sqlite3.connect(arg)
 		self.cursor=self.conn.cursor()
 		self.cursor.execute('CREATE TABLE IF NOT EXISTS funcDep(nameTable TEXT NOT NULL,lhs TEXT NOT NULL,rhs TEXT NOT NULL, PRIMARY KEY(nameTable,lhs,rhs))')
 		self.conn.commit() 
 
+
 	def do_insert(self,arg):#insert est un triplet (nameTable,lhs,rhs)
-		self.cursor.execute("INSERT INTO funcDep(nameTable,lhs,rhs) VALUES(?,?,?)",arg[1:4])
+		res = parse(arg)
+		self.cursor.execute("INSERT INTO funcDep(nameTable,lhs,rhs) VALUES(?,?,?)",(res[0],res[1],res[2]))
 		self.conn.commit()
-	
 
 	def do_delete(self,arg):	
-		self.cursor.execute("DELETE FROM funcDep WHERE nameTable=? AND lhs=? AND rhs=?",arg[1:3])
+		res = parse(arg)
+		self.cursor.execute("DELETE FROM funcDep WHERE nameTable=? AND lhs=? AND rhs=?",(res[0],res[1],res[2]))
 		self.conn.commit()
+
+
 	def do_display(self,arg):
 		'Display the funcDep table of the database actually connected'
-<<<<<<< HEAD
 		for row in self.cursor.execute("SELECT * from funcDep ORDER BY nameTable"):
-			print(row)
+			print(row)	
 
-=======
-		for row in cursor.execute("SELECT * from funcDep ORDER BY nameTable"):
-			print(row)
->>>>>>> parent of be5d80c... Kitty kat
+	def do_getLHS(self,arg):
+		self.cursor.execute("SELECT lhs FROM funcDep WHERE nameTable =?",parse(arg))
+		res = self.cursor.fetchall()
+		print(res)
+
+	def do_getRHS(self,arg):
+		self.cursor.execute("SELECT rhs FROM funcDep WHERE nameTable =?",parse(arg))
+		res = self.cursor.fetchall()
+		print(res)
+
+
+
 def parse(arg):
     'Convert a series of zero or more numbers to an argument tuple'
-    return tuple(map(int, arg.split()))
+    return tuple(map(str, arg.split(" ")))
 
 if __name__ == '__main__':
 	dBHandler().cmdloop()
