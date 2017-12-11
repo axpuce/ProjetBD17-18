be5d80ce83be8de81a17a3bc9b2d87403d473f7d
Unknown <yacine.sahli@student.umons.ac.be>
2017-12-11 14:43:31 +0100

    Kitty kat

--- dBHandler.py
+++ dBHandler.py
@@ -10,26 +10,27 @@ class dBHandler(cmd.Cmd) :
 		self.conn = None
 	def do_connect(self,arg):
 		'Attempt to connect to the specified database'
-		self.conn=sqlite3.connect(arg[1])
+		self.conn=sqlite3.connect(arg)
 		self.cursor=self.conn.cursor()
-		self.cursor.execute('CREATE TABLE IF NOT EXISTS funcDep(nameTable TEXT NOT NULL,lhs TEXT NOT NULL,rhs TEXT NOT NULL, PRIMARY KEY(nameTable,lhs,rhs))')
+		self.cursor.execute("CREATE TABLE IF NOT EXISTS funcDep(nameTable TEXT NOT NULL,lhs TEXT NOT NULL,rhs TEXT NOT NULL, PRIMARY KEY(nameTable,lhs,rhs))")
 		self.conn.commit() 
 
-	def do_insert(self,arg):#insert est un triplet (nameTable,lhs,rhs)
-		self.cursor.execute("INSERT INTO funcDep(nameTable,lhs,rhs) VALUES(?,?,?)",arg[1:4])
+	def do_insert(self,arg):
+		'insert prend arg en argument qui est un triplet (nameTable,lhs,rhs)'
+		self.cursor.execute("INSERT INTO funcDep(nameTable,lhs,rhs) VALUES(?,?,?)",arg)
 		self.conn.commit()
-	
 
 	def do_delete(self,arg):	
-		self.cursor.execute("DELETE FROM funcDep WHERE nameTable=? AND lhs=? AND rhs=?",arg[1:3])
+		self.cursor.execute("DELETE FROM funcDep WHERE nameTable=? AND lhs=? AND rhs=?",arg)
 		self.conn.commit()
+
 	def do_display(self,arg):
 		'Display the funcDep table of the database actually connected'
-		for row in cursor.execute("SELECT * from funcDep ORDER BY nameTable"):
+		for row in self.cursor.execute("SELECT * FROM funcDep ORDER BY nameTable"):
 			print(row)
-def parse(arg):
-    'Convert a series of zero or more numbers to an argument tuple'
-    return tuple(map(int, arg.split()))
+	def do_getLHS(self,arg):
+		print(self.cursor.execute("SELECT lhs FROM funcDep WHERE nameTable = ?",arg))		
+
 
 if __name__ == '__main__':
 	dBHandler().cmdloop()
