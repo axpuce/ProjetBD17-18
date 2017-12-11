d0b3e6fe29f31243caea0b2a167f4973c09ab674
Unknown <yacine.sahli@student.umons.ac.be>
2017-12-11 11:01:32 +0100

    corr

--- dBHandler.py
+++ dBHandler.py
@@ -4,17 +4,17 @@ class dBHandler :
 	def __init__(self,database):
 		self.conn=sqlite3.connect(database)
 		self.cursor=self.conn.cursor()
-		self.cursor.execute("""CREATE TABLE IF NOT EXISTS funcDep(nameTable TEXT NOT NULL,lhs TEXT NOT NULL,rhs TEXT NOT NULL, PRIMARY KEY(nameTable,lhs,rhs))""")
+		self.cursor.execute('CREATE TABLE IF NOT EXISTS funcDep(nameTable TEXT NOT NULL,lhs TEXT NOT NULL,rhs TEXT NOT NULL, PRIMARY KEY(nameTable,lhs,rhs))')
 		self.conn.commit()
 	
 
 	def insert(self,insert):#insert est un triplet (nameTable,lhs,rhs)
-		self.cursor.execute("""INSERT INTO funcDep(nameTable,lhs,rhs) VALUES(?,?,?)""",insert)
+		self.cursor.execute("INSERT INTO funcDep(nameTable,lhs,rhs) VALUES(?,?,?)",insert)
 		self.conn.commit()
 	
 
 	def delete(self,remove):	
-		self.cursor.execute("""DELETE FROM funcDep WHERE nameTable=? AND lhs=? AND rhs=?""",remove)
+		self.cursor.execute("DELETE FROM funcDep WHERE nameTable=? AND lhs=? AND rhs=?",remove)
 		self.conn.commit()
 
 
